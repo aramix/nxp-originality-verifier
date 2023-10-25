e44d47ce1ba0854e0c4b7bd195472031b04aff94
Aram Bayadyan <aram.bv@gmail.com>
2023-10-25 23:23:09 +0400

    updated readme and tests to use cascading

--- README.md
+++ README.md
@@ -7,8 +7,7 @@ Verifies originality of NTAG, Mifare UL or MIFARE UL EV1 tags
 ## Usage
 
 ```dart
-final verifier = OriginalityVerifier();
-verifier.verifyOriginality(
+OriginalityVerifier().verifyOriginality(
         Uint8List.fromList([4, 171, 135, 242, 202, 17, 144]),
         Uint8List.fromList(
             [103, 177, 117, 11, 204, 235, 17, 116, 249, 163, 59, 242, 14, 2, 38, 85, 26, 99, 102, 147, 155, 49, 249, 73, 132, 140, 60, 8, 115, 80, 250, 144]));
@@ -17,9 +16,8 @@ verifier.verifyOriginality(
 Or specify your own (given by your manufacturer) list of keys
 
 ```dart
-final verifier = OriginalityVerifier(
-        ['04494e1a386d3d3cfe3dc10e5de68a499b1c202db5b132393e89ed19fe5be8bc61']);
-verifier.verifyOriginality(
+OriginalityVerifier(
+        ['04494e1a386d3d3cfe3dc10e5de68a499b1c202db5b132393e89ed19fe5be8bc61'])verifyOriginality(
         Uint8List.fromList([4, 171, 135, 242, 202, 17, 144]),
         Uint8List.fromList(
             [103, 177, 117, 11, 204, 235, 17, 116, 249, 163, 59, 242, 14, 2, 38, 85, 26, 99, 102, 147, 155, 49, 249, 73, 132, 140, 60, 8, 115, 80, 250, 144]));
--- test/nxp_originality_verifier_test.dart
+++ test/nxp_originality_verifier_test.dart
@@ -4,17 +4,15 @@ import 'package:nxp_originality_verifier/nxp_originality_verifier.dart';
 
 void main() {
   test('check for originality against known public keys', () {
-    final verifier = OriginalityVerifier();
     expect(
-        verifier.verifyOriginality(
+        OriginalityVerifier().verifyOriginality(
             Uint8List.fromList([4, 171, 135, 242, 202, 17, 144]),
             Uint8List.fromList([103, 177, 117, 11, 204, 235, 17, 116, 249, 163, 59, 242, 14, 2, 38, 85, 26, 99, 102, 147, 155, 49, 249, 73, 132, 140, 60, 8, 115, 80, 250, 144])), true);
   });
   test('check for originality against a list of given public keys', () {
-    final verifier = OriginalityVerifier(
-        ['04494e1a386d3d3cfe3dc10e5de68a499b1c202db5b132393e89ed19fe5be8bc61']);
     expect(
-        verifier.verifyOriginality(
+        OriginalityVerifier(
+        ['04494e1a386d3d3cfe3dc10e5de68a499b1c202db5b132393e89ed19fe5be8bc61']).verifyOriginality(
             Uint8List.fromList([4, 171, 135, 242, 202, 17, 144]),
             Uint8List.fromList([103, 177, 117, 11, 204, 235, 17, 116, 249, 163, 59, 242, 14, 2, 38, 85, 26, 99, 102, 147, 155, 49, 249, 73, 132, 140, 60, 8, 115, 80, 250, 144])), true);
   });
